351d2f5e06f889db5de4490fc5c7bab6b003e5b9
Shadow Walker <franklynpeter2006@gmail.com>
2026-03-16 04:30:07 +0100

    feat: add edit categories modal

---
+++ resources/views/components/icons/sparkles.blade.php
@@ -0,0 +1,3 @@
+<svg aria-hidden="true" viewBox="0 0 24 24" class="h-5 w-5" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M13 3l2 2-6 6-2 4 4-2 6-6 2 2-8 8H3v-8l8-8Z" />
+</svg>
